bfc2186f834b5b225cfbdb34be3e2f30db8e58f6
Matias Pitkänen <M2tias@Gmail.com>
2022-06-18 11:10:08 +0300

    Book UI work

---
+++ Books/Assets/Scripts/BookLine.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BookLine : MonoBehaviour
+{
+    private List<string> alphabetUpper = new List<string>() { "Æ", "Σ", "Ω", "Ц", "З", "Я", "I", "Y", "E", "Ջ", "Ի", "Ԓ", "Ϩ", "Ϫ", "Є", "Э", "Ө", "Մ", "Ӯ", "Ӑ", "Ծ", "Ӵ", "Ж", "Λ", "Ҁ" };
+    private List<string> alphabetLower = new List<string>() { "æ", "σ", "ω", "ц", "з", "я", "i", "y", "e", "ջ", "ի", "ԓ", "ϩ", "ϫ", "є", "э", "ө", "մ", "ӯ", "ӑ", "ծ", "ӵ", "ж", "λ", "ҁ" };
+    private string line;
+
+    [SerializeField]
+    private bool randomWord;
+
+    [SerializeField]
+    private int lineLength;
+
+    [SerializeField]
+    private Text uiText;
+
+    [SerializeField]
+    private int minWordLength;
+
+    [SerializeField]
+    private int maxWordLength;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (randomWord)
+        {
+            GenerateRandomWord();
+        }
+        else
+        {
+            uiText.text = "This is a hotspot! :O";
+            uiText.color = new Color(0, 0, 0);
+        }
+    }
+
+    private void GenerateRandomWord()
+    {
+        int charsLeft = lineLength;
+        bool firstWord = true;
+        while (charsLeft > 0)
+        {
+            int wordLen = Random.Range(minWordLength, maxWordLength);
+            charsLeft -= wordLen + 1; // space between words
+            string firstLetter = firstWord ? alphabetUpper[Random.Range(0, alphabetUpper.Count)] : alphabetLower[Random.Range(0, alphabetUpper.Count)];
+            firstWord = false;
+
+            StringBuilder word = new StringBuilder(firstLetter);
+
+            for (int i = 1; i < wordLen; i++)
+            {
+                word.Append(alphabetLower[Random.Range(0, alphabetUpper.Count)]);
+            }
+
+            word.Append(" ");
+            line += word;
+        }
+
+        uiText.text = line;
+        uiText.color = new Color(0.25f, 0.25f, 0.25f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
